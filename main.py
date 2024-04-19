5bfe4e2a9e6ce93cd008848c5b6b37f817fc8c17
Jitendra Nayak <jitendra.nayak@petonic.in>
2024-04-19 09:29:22 +0530

    test

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+## sudo pipenv install llama-index-readers-file
+## sudo pipenv install llama-index-readers-web
+from llama_index import SimpleWebPageReader
+
 import os
 from dotenv import load_dotenv
+# from llama_index import SimpleWebPageReader,VectorStoreIndex     # outdated now
+from llama_index.readers.web import SimpleWebPageReader
+from llama_index.core import VectorStoreIndex
+# from llama_index.core.readers.file.base import SimpleWebPageReader
+
 
 if __name__ == "__main__":
-    load_dotenv
+    load_dotenv()
     print ("Hello world llama-index course")
     print (f'OPENAI_API_KEY : {os.environ["OPENAI_API_KEY"]}')
